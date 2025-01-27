42809ed35cbdf2714507035f963dc5d4421c7318
Nour Wahed <Nourwahed1001@gmail.com>
2025-01-28 00:29:45 +0200

    counter words

--- 6.py
+++ 6.py
@@ -1,7 +1,7 @@
 def count_word(string):
     count = 1
     sr = list(string)
-    for i in range(len(sr)) :
+    for i in range(len(sr)):
         if sr[i] == " " :
             count += 1
     print(count)
