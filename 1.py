f794d37d8c6bf3432457ec9907426319c94ebc0b
Nour Wahed <Nourwahed1001@gmail.com>
2025-01-29 03:22:33 +0200

    f2

--- 1.py
+++ 1.py
@@ -2,32 +2,50 @@ import os
 import platform
 
 p = print
-plat = platform.system()
-if plat == "Windows" :
-    os.system("cls")
-elif plat == "Linux" :
-    os.system("clear")
-else :
-    p("not_support") 
-
-def calculator(num1 , num2 , opra):
-
-    if opra == "+" :
-        return num1 + num2
-    elif opra == "-" :
-        return num1 - num2
-    elif opra == "*" :
-        return num1 * num2
-    elif opra == "/" :
-        return num1 / num2
-    elif opra == "%" :
-        return num1 % num2
-    elif opra == "//" :
-        return num1 // num2
+def clear_screen():
+    plat = platform.system()
+    if plat == "Windows" :
+        os.system("cls")
+    elif plat == "Linux" :
+        os.system("clear")
     else :
-        return "please try agin"
+        p("not_support") 
+
+def re_fun():
+    while 1 :
+        ch = input("1.Contiune or 2.Exit :> ")
+        if ch == "1" :
+            clear_screen()
+            inp1 = int(input("Enter frist number >>> "))
+            inp2 = input("Enter opration >>> ")
+            inp3 = int(input("Enter secound number >>> "))
+            p (f"the result : {calculator(inp1,inp3,inp2)}")
+        elif ch == "2" :
+            exit() 
+        else :
+            p("try again")   
+            
+def calculator(num1 , num2 , opra):
+    try :
+        if opra == "+" :
+            return num1 + num2
+        elif opra == "-" :
+            return num1 - num2
+        elif opra == "*" :
+            return num1 * num2
+        elif opra == "/" :
+            return num1 / num2
+        elif opra == "%" :
+            return num1 % num2
+        elif opra == "//" :
+            return num1 // num2
+        else :
+            return "please try agin"
+    except ValueError:
+        print("you input is uncorrect , please try again")
 
 inp1 = int(input("Enter frist number >>> "))
 inp2 = input("Enter opration >>> ")
 inp3 = int(input("Enter secound number >>> "))
 p (f"the result : {calculator(inp1,inp3,inp2)}")
+re_fun()
