4ddc730b90640495d9be65272f6aef99b9dd3439
nouewahed <Nourwahed1001@gmail.com>
2025-02-07 00:54:24 +0200

    ~fun~

--- 12.py
+++
@@ -1,7 +0,0 @@
-def isVoild(passw):
-        if   passw.isdigit() and len(passw) == 4:
-            return (True)
-        else :
-            return (False)
-        
-print(isVoild("3868")) # true
