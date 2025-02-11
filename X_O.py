34d40da0b83ae9ebde3caccf5e8fc193b76cbc53
nouewahed <Nourwahed1001@gmail.com>
2025-02-11 14:12:20 +0200

    lesa

--- X_O.py
+++ X_O.py
@@ -1,14 +1,6 @@
 import platform
 import os
 class x_o :
-    list = [1,2,3,4,5,6,7,8,9]
-    sc = f"""
-    {list[0]} | {list[1]} | {list[2]}
-    ----------
-    {list[3]} | {list[4]} | {list[5]}
-    ----------
-    {list[6]} | {list[7]} | {list[8]}
-    """
     _x_o_= """ 
 
     Welcome in game XO
@@ -33,38 +25,40 @@ class x_o :
             os.system("clear")
         else:
             print("not support")
-
-    def xc(self):
-
-        return self.player1 , self.player2
-
-i = 0
-game = x_o("X")
-game.clean()
-print(game._x_o_)
-pt = input(":> ")
-if pt == "1" :
-    user1 = input("Enter Your X or O : ")
-    game = x_o(user1)
-    game.clean()
-    print(game.sc)
-    # while i <= 9 :
-    play_one = int(input(f"{game.player1} :>> "))
-    game.list[play_one-1] = game.player1
-    play_two = int(input(f"{game.player2} :>> ")) 
-    game.list[play_two-1] = game.player2
-    game.clean()
-    print(game.sc)
-    i += 1
-elif pt == "2":
-    exit()
-else :
-    while pt != "1" or pt != "2" :
+    @staticmethod
+    def main():
+        game = x_o("X")
+        game.clean()
+        print(game._x_o_)
         pt = input(":> ")
         if pt == "1" :
             user1 = input("Enter Your X or O : ")
             game = x_o(user1)
             game.clean()
-            print(game.sc)
+            for i in range(4):
+                list_range = [1,2,3,4,5,6,7,8,9]
+                sc = f"""
+                    {list_range[0]} | {list_range[1]} | {list_range[2]}
+                    ----------
+                    {list_range[3]} | {list_range[4]} | {list_range[5]}
+                    ----------
+                    {list_range[6]} | {list_range[7]} | {list_range[8]}
+                """
+                play_one = int(input(f"player 1 {game.player1} :>> "))
+                list_range[play_one-1] = game.player1
+                play_two = int(input(f"player 2 {game.player2} :>> ")) 
+                list_range[play_two-1] = game.player2
+                game.clean()
+                print(sc)
         elif pt == "2":
             exit()
+        else :
+            while pt != "1" or pt != "2" :
+                print("you input is hame mistake ! , please try again")
+                pt = input(":> ")
+                if pt == "1" :
+                    game.main()
+                elif pt == "2":
+                    exit()
+
+x_o("x").main()
