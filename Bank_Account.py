f23629f32776bd6b699e51f1152ecb1b4b2d1fc9
nouewahed <Nourwahed1001@gmail.com>
2025-02-22 20:44:06 +0200

    not

--- Bank_Account.py
+++ Bank_Account.py
@@ -3,6 +3,7 @@ import platform
 import datetime
 import os
 import time
+
 p = print
 
 class Bank():
@@ -37,29 +38,6 @@ class Bank():
             os.system("clear")
         else:
             p("not support !!")
-
-    def read(self , info):
-        self.info = info
-        list_id = []
-        with open("user_data.json" , "r+") as rd :
-            j_r = json.load(rd)
-            for keys in j_r.keys():
-                list_id.append(keys)
-            if info == "name" :
-                for i in range(len(list_id)):
-                    p(f"{i+1} : { j_r[list_id[i]]['name'] }")
-            elif info == "id" :
-                for i in range(len(list_id)):
-                    p(f"{i+1} : { j_r[list_id[i]]['id'] }") 
-            elif info == "accout" :
-                for i in range(len(list_id)):
-                    p(f"{i+1} : { list_id[i] }")
-            elif info == "sp_id" :
-                yo_se = input("Enter you number to git info :> ")
-                p(f">> { j_r[yo_se]}")
-            else :
-                exit()        
-            rd.close()
                 
     @staticmethod
     def add_accout():
@@ -79,13 +57,20 @@ class Bank():
             val_list.append(val)
         for v in range(len(keys_list)):
             p(f"{keys_list[v]} : {val_list[v]}")
-        con = input("Are Sure to add this new account in system 1.yes 2.no :>")
+        con = input("Are Sure to add this new account in system 1.yes 2.no 3.Return 4.Exit :>")
         if con == "1" :
             fi =  open("user_data.json" , "r+")
-            json.dump(data[num],fi,indent=4)
+            json.dump(data[num],fi,indent=4) # Edit
         elif con == "2" :
+            Bank.clear_screen()
+            p("try again ... ")
+            time.sleep(2)
+            Bank.add_accout()
+        elif con == "3" :
             Bank.main()
-    
+        elif con == "4" :
+            exit()
+
     @staticmethod
     def main():
         Bank.clear_screen()
@@ -147,8 +132,13 @@ class Bank():
                     elif r_v == "2" :
                         exit()
                     else :
-                        p("you input is erro")
-                        exit()
+                        while r_v not in range (1,3):
+                            p("you input is erro")
+                            r_v = input("1.Return 2.Exit :> ")
+                            if r_v == "1" :
+                                Bank.main()
+                            elif r_v == "2" :
+                                exit()
                 elif acc_fu == "unban" :
                     da[acc_ba]['block'] = False
                     with open("user_data.json" , "w+") as file2 :
@@ -161,7 +151,12 @@ class Bank():
                         exit()
                     else :
                         p("you input is erro")
-                        exit()
+                        while r_v not in range(1,3):
+                            r_v = input("1.Return 2.Exit :> ")
+                            if r_v == "1" :
+                                Bank.main()
+                            elif r_v == "2" :
+                                exit()              
                 else:
                     p("you input in mistake , try again")
                     time.sleep(1)
@@ -171,6 +166,87 @@ class Bank():
 
             elif in_ve == "4" :
                 exit()
+            
+            else :
+                while in_ve not in [1,2,3,4] :
+                    p("you input is erro , please try agains!!")
+                    in_ve = input("::>> ")
+                    if in_ve == "1"  :
+                        def sc2():
+                            nums_list = []
+                            name_list= []
+                            id_list = []
+                            Bank.clear_screen()
+                            file3 = open("user_data.json" , "r+")
+                            rd = json.load(file3)
+                            for nums in rd.keys():
+                                nums_list.append(nums)
+                                for names in rd[nums]["name"] :
+                                    name_list.append(rd[nums]["name"])
+                                    break
+                                for ids in rd[nums]["id"] :
+                                    id_list.append(rd[nums]["id"])
+                                    break
+                            for na , nu , i in zip(name_list , nums_list , id_list) :
+                                p("_"*50+f"""\n{na}\t:|:\t{nu}\t:|:\t{i}""")
+                            con2 = input("\nEnter 1.Return 2.Exit :> ")
+                            if con2 == "1" :
+                                Bank.main()  # edit again 
+                            elif con2 == "2" :
+                                exit()
+                            else:
+                                while con2 not in [1,2] :
+                                    con2 = input("Enter 1.Return 2.Exit :> ")
+                                    if con2 == "1" :
+                                        Bank.main()
+                                    elif con2 == "2" :
+                                        exit()    
+                        sc2()                    
+                    elif in_ve == "2" :
+                        Bank.clear_screen()
+                        file = open("user_data.json" , "+r")
+                        da = json.load(file)
+                        acc_ba = input("Enter number of accont to has ban or remove :> ")
+                        if da[acc_ba]['block'] == False :
+                            p("this account is not has ban")
+                        else:
+                            p("this account has ban")
+                        acc_fu = input("ban or unban :> ")
+                        if acc_fu == "ban" :
+                            da[acc_ba]["block"] = True
+                            with open("user_data.json" , "w+") as file2 :
+                                json.dump(da , file2 , indent=4)
+                            file.close()
+                            r_v = input("1.Return 2.Exit :> ")
+                            if r_v == "1" :
+                                Bank.main()
+                            elif r_v == "2" :
+                                exit()
+                            else :
+                                p("you input is erro")
+                                exit()
+                        elif acc_fu == "unban" :
+                            da[acc_ba]['block'] = False
+                            with open("user_data.json" , "w+") as file2 :
+                                json.dump(da , file2 , indent=4)
+                            file.close()
+                            r_v = input("1.Return 2.Exit :> ")
+                            if r_v == "1" :
+                                Bank.main()
+                            elif r_v == "2" :
+                                exit()
+                            else :
+                                p("you input is erro")
+                                exit()
+                        else:
+                            p("you input in mistake , try again")
+                            time.sleep(1)
+                            Bank.main()
+                    elif in_ve == "3" :
+                        Bank.main()
+
+                    elif in_ve == "4" :
+                        exit()
 
         elif in_us == "2" :
             Bank.add_accout()
@@ -179,24 +255,8 @@ class Bank():
         else:
             while in_us not in [1,2,3] :
                 p("you make a mistake in you input , please try again !!")
-                in_us = input("::> ")
-                if in_us == "1" :
-                    Bank.clear_screen()
-                    p(Bank.sc_2)
-                    in_ve = input("::>> ")
-                    if in_ve == "1" :
-                        Bank.clear_screen()
-                        Bank().read("id")
-                    elif in_ve == "2" :
-                        Bank.clear_screen()
-                        Bank().read("name")
-                    elif in_ve == "3" :
-                        Bank.main()
-                    elif in_ve == "4" :
-                        exit()
-                elif in_us == "2" :
-                    Bank.add_accout()
-                elif in_us == "3" :
-                    exit()
+                time.sleep(2)
+                Bank.main()
+
 
 Bank().main()
